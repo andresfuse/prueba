d6af88b4636a4188fd0de9e69b894cc9c15c38cc
andresfuse <42395274+andresfuse@users.noreply.github.com>
2018-11-26 19:06:20 -0500

    Update cod.js

--- cod.js
+++ cod.js
@@ -1,4 +1,4 @@
-function pintar(datos, variable){
+function barplot(datos, variable){
   var margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
